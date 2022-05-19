452668b7199a5b8a1308dddeff0435f8126f85cc
rasedul99 <rasedul2199@gmail.com>
2022-05-19 07:37:43 +0600

    env file added

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://dbuser1:VOsRhjx0Nukp7lYU@cluster1.tqsec.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.tqsec.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
