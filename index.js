1ec37271c67d2b408debc3d7fca395dfb5e76ed5
rasedul99 <rasedul2199@gmail.com>
2022-05-19 07:27:50 +0600

    update & delete endpoint

--- index.js
+++ index.js
@@ -1,9 +1,99 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT | 5000;
+const ObjectId = require("mongodb").ObjectId;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://dbuser1:VOsRhjx0Nukp7lYU@cluster1.tqsec.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+const run = async () => {
+  try {
+    await client.connect();
+    const carsCollection = client.db("carInventory").collection("allcars");
+    app.post("/carHouse", async (req, res) => {
+      const cars = req.body;
+      if (!cars.productname | !cars.image | !cars.price) {
+        return res.send({ success: false, error: "Please provide all info" });
+      }
+      const result = await carsCollection.insertOne(cars);
+      res.send({
+        success: true,
+        message: `inserted succesfully ${cars.productname}`,
+      });
+    });
+
+    app.get("/allcars", async (req, res) => {
+      const cursor = carsCollection.find();
+      const cars = await cursor.toArray();
+
+      if (!cars?.length) {
+        return res.send({ success: false, message: "No cars found" });
+      }
+      res.send({ success: true, data: cars });
+    });
+    app.get("/carhouse/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      // console.log(query);
+      const result = await carsCollection.findOne(query);
+      res.send({ success: true, result: result });
+    });
+    app.put("/carupdate/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const updateStock = req.body;
+      console.log(updateStock);
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          quantity: updateStock.newquantity,
+        },
+      };
+      const result = await carsCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
+    app.put("/carhouse/solditemupdate/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const updateSoldItem = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          soldItem: updateSoldItem.newSoldItem,
+          quantity: updateSoldItem.newStock,
+        },
+      };
+      const result = await carsCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
+
+    app.delete("/car/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await carsCollection.deleteOne(query);
+      res.send(result);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+run();
 
 app.get("/", (req, res) => {
-  res.send("Hello Worl!");
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
